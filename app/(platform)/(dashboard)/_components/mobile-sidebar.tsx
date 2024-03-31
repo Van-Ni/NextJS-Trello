77271b46d94e57df1649687c507755187f04d25f
Van Ni <vanni4018@gmail.com>
2024-03-31 20:54:54 +0700

    Sidebar

---
+++ app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useMobileSidebar } from "@/app/hooks/use-sidebar-mobile";
+import { Button } from "@/components/ui/button";
+import { SheetContent, Sheet } from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+import { useEffect, useState } from "react";
+import { Sidebar } from "./sidebar";
+import { usePathname } from "next/navigation";
+
+
+export const MobileSidebar = () => {
+    const pathname = usePathname();
+    
+    const [isMounted, setIsMounted] = useState(false);
+
+    const onOpen = useMobileSidebar((state) => state.onOpen);
+    const onClose = useMobileSidebar((state) => state.onClose);
+    const isOpen = useMobileSidebar((state) => state.isOpen);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+    useEffect(() => {
+        onClose();
+    }, [pathname, onClose]);
+
+    if (!isMounted) {
+        return null;
+    }
+
+    return (
+        <>
+            <Button
+                onClick={onOpen}
+                className="block md:hidden mr-2"
+                variant="ghost"
+                size="sm"
+            >
+                <Menu className="h-4 w-4" />
+            </Button>
+            <Sheet open={isOpen as boolean} onOpenChange={onClose}>
+                <SheetContent
+                    side="left"
+                    className="p-2 pt-10"
+                >
+                    <Sidebar
+                        storageKey="t-sidebar-mobile-state"
+                    />
+                </SheetContent>
+            </Sheet>
+        </>
+    )
+};
