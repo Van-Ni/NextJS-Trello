9a18e752e887bf7b0422a19d2ae60518346d204e
Van Ni <vanni4018@gmail.com>
2024-04-07 10:05:22 +0700

    List component

---
+++ type.ts
@@ -0,0 +1,7 @@
+// TypeScript Intersection Types
+
+import { Card, List } from "@prisma/client";
+
+export type ListWithCards = List & { cards: Card[] };
+
+export type CardWithList = Card & { list: List };
