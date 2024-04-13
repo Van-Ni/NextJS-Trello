7ca54d8f3fe2379cbd735dff4ffc5a4fea7ca609
Van Ni <vanni4018@gmail.com>
2024-04-13 07:35:15 +0700

    Card model

--- app/(platform)/(dashboard)/board/[boardId]/_components/card/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card/card-item.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useCardModal } from "@/components/hooks/use-card-modal";
 import { Draggable } from "@hello-pangea/dnd";
 import { Card } from "@prisma/client";
 
@@ -13,7 +14,7 @@ export const CardItem = ({
     data,
     index,
 }: CardItemProps) => {
-    // const cardModal = useCardModal();
+    const cardModal = useCardModal();
 
     return (
         <Draggable draggableId={data.id} index={index}>
@@ -23,7 +24,7 @@ export const CardItem = ({
                     {...provided.dragHandleProps}
                     ref={provided.innerRef}
                     role="button"
-                    // onClick={() => cardModal.onOpen(data.id)}
+                    onClick={() => cardModal.onOpen(data.id)}
                     className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
                 >
                     {data.title}
